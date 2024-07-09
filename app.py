0e6f93e0f26b9f50ef63589c0156a2ae8b347464
HyeongMinK <aalynm@gmail.com>
2024-07-09 23:53:17 +0900

    hi

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import streamlit as st
 from streamlit_mic_recorder import mic_recorder
 import whisper
-import openai
+import soundfile as sf
+import numpy as np
+import io
 import tempfile
+from openai import OpenAI
 import os
 
 # Load the Whisper model
 model = whisper.load_model("base")
 api_key = os.getenv('OPENAI_API_KEY')  # 환경 변수에서 API 키를 가져옵니다.
+client = OpenAI(api_key=api_key)
 if not api_key:
     raise ValueError("OPENAI_API_KEY 환경 변수가 설정되지 않았습니다.")
-openai.api_key = api_key
 
 def transcribe_audio(audio):
     # Save audio to a temporary file
@@ -23,18 +26,18 @@ def transcribe_audio(audio):
         return result['text']
 
 def gpt_call(client, text):
-    completion = openai.ChatCompletion.create(
-        model="gpt-4",
+    completion = client.chat.completions.create(
+        model="gpt-4o",
         messages=[
             {"role": "system", "content": "Your only task is to translate English to Korean. Do not write anything other than the translation."},
             {"role": "user", "content": text}
         ]
     )
-    return completion.choices[0].message['content']
+    return completion.choices[0].message.content
 
 def text_to_speech(client, text):
-    response = openai.Audio.create(
-        model="whisper-1",
+    response = client.audio.speech.create(
+        model="tts-1",
         voice="echo",
         input=text
     )
@@ -52,14 +55,14 @@ audio = mic_recorder(start_prompt="Start recording", stop_prompt="Stop recording
 if audio:
     st.audio(audio['bytes'], format='audio/webm')
     transcription = transcribe_audio(audio)
-    ts_text = gpt_call(openai, transcription)
+    ts_text = gpt_call(client, transcription)
     st.write("Transcription:")
     st.write(transcription)
     st.write("Translation:")
     st.write(ts_text)
 
     # Convert translated text to speech
-    tts_audio_file = text_to_speech(openai, ts_text)
+    tts_audio_data = text_to_speech(client, ts_text)
     
     # Play the TTS audio
-    st.audio(tts_audio_file, format='audio/mp3')
+    st.audio(tts_audio_data, format='audio/wav')
