673bcdbae6e448d61beb3bf04389a9793c0c7668
HyeongMinK <aalynm@gmail.com>
2024-07-09 23:20:04 +0900

    hi

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ import soundfile as sf
 import numpy as np
 import io
 import tempfile
+from openai import OpenAI
 
 # Load the Whisper model
 model = whisper.load_model("base")
+api_key = os.getenv('OPENAI_API_KEY')  # 환경 변수에서 API 키를 가져옵니다.
+if not api_key:
+    raise ValueError("OPENAI_API_KEY 환경 변수가 설정되지 않았습니다.")
+
 
 def transcribe_audio(audio):
     # Save audio to a temporary file
@@ -18,6 +23,16 @@ def transcribe_audio(audio):
         # Transcribe audio using Whisper
         result = model.transcribe(tmp_wav_file.name)
         return result['text']
+def gpt_call(text):
+  client = OpenAI(api_key=api_key)
+  completion = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    {"role": "system", "content": "Your only task is to be a translator and translate English to Korean."},
+    {"role": "user", "content": f"text = {text}"}
+  ]
+)
+  return completion.choices[0].message.content
 
 # Streamlit interface
 st.title("Audio Recording and Transcription with Whisper")
@@ -31,3 +46,5 @@ if audio:
     transcription = transcribe_audio(audio)
     st.write("Transcription:")
     st.write(transcription)
+    st.write("Translation:")
+    st.write(gpt_call(transcription))
