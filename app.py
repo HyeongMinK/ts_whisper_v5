36475e45a74ed243d2982d4f8ecaf4bb954798e7
HyeongMinK <aalynm@gmail.com>
2024-07-17 20:07:05 +0900

    v3

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ if st.session_state.is_recording == True:
 
 st.sidebar.title("Recordings")
 
-if st.session_state.once_recording == True and not st.session_state.transcriptions:
+if st.session_state.once_recording == True and st.session_state.transcriptions:
 
 
     for i in range(len(st.session_state.transcriptions)):
