ecc4f69ec1ed5be7a0da82bf4b1ee541c29c03b8
HyeongMinK <aalynm@gmail.com>
2024-07-18 01:07:09 +0900

    v4

--- app.py
+++ app.py
@@ -216,7 +216,6 @@ with col2_file_uploader:
     elif len(uploaded_files) < len(st.session_state.uploader_list):
         st.session_state.uploader = False
         unique_to_list = [item for item in st.session_state.uploader_list if item not in uploaded_files]
-        st.write(f"OpenAI에서 파일 삭제: {unique_to_list}")
         st.session_state.uploader_list = uploaded_files
 
         # OpenAI API를 통해 파일 리스트 조회
@@ -225,13 +224,11 @@ with col2_file_uploader:
             file_list_data = file_list
             vector_store_files = client.beta.vector_stores.files.list(vector_store_id=st.session_state.vector_store_id)
 
-            # unique_to_list에 있는 파일들 openai에서 삭제
-            for file_name in unique_to_list:
-                for file in file_list_data:
-                    if file.filename == file_name:
-                        client.beta.vector_stores.files.delete(vector_store_id=st.session_state.vector_store_id, file_id=file.id)
-                        client.files.delete(file.id)
-                        st.write(f"OpenAI에서 파일 삭제: {file_name}")
+            for file in file_list_data:
+                if file.filename == unique_to_list.name:
+                    client.beta.vector_stores.files.delete(vector_store_id=st.session_state.vector_store_id, file_id=file.id)
+                    client.files.delete(file.id)
+                    st.write(f"OpenAI에서 파일 삭제: {file_name}")
 
         except Exception as e:
             st.write(f"파일 삭제 중 오류가 발생했습니다: {file_name}")
