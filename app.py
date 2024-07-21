8be8d0fbe161da7a751a4249a21c30e08b844425
HyeongMinK <aalynm@gmail.com>
2024-07-21 22:42:32 +0900

    v5

--- app.py
+++ app.py
@@ -8,39 +8,6 @@ import warnings
 from pydub import AudioSegment
 import time
 
-# 하단 고정 텍스트와 스타일 조정
-st.markdown(
-    """
-    <style>
-    @import url('https://fonts.googleapis.com/css2?family=Futura:wght@300;400;700&display=swap');
-
-    .footer {
-        position: relative;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        background-color: #734954;
-        color: #F2F2F2;
-        text-align: right;
-        padding: 10px;
-        border-top: 1px solid #F2F2F2;
-        font-family: 'Futura', sans-serif; /* 폰트 패밀리 변경 */
-        font-size: 12px; /* 폰트 크기 변경 */
-        font-style: italic; /* 폰트 스타일 변경 */
-    }
-    section[data-testid="stSidebar"] {
-        width: 150px !important; # Set the width to your desired value
-    }
-    </style>
-    <div class="footer">
-        Digital Wellness Lab 2024<br>
-        Business Analytics, School of Management<br>
-        Kyung Hee University<br>
-        Maintained by H-.M-. Kim & S-.W-. Kim
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
 # Suppress FP16 warning
 warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
@@ -465,6 +432,39 @@ if st.session_state.once_recording == True and st.session_state.transcriptions:
                         st.session_state.temp_page = change_option + 1
                         st.rerun()                    
 
+# 하단 고정 텍스트와 스타일 조정
+st.markdown(
+    """
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Futura:wght@300;400;700&display=swap');
+
+    .footer {
+        position: relative;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #734954;
+        color: #F2F2F2;
+        text-align: right;
+        padding: 10px;
+        border-top: 1px solid #F2F2F2;
+        font-family: 'Futura', sans-serif; /* 폰트 패밀리 변경 */
+        font-size: 12px; /* 폰트 크기 변경 */
+        font-style: italic; /* 폰트 스타일 변경 */
+    }
+    section[data-testid="stSidebar"] {
+        width: 150px !important; # Set the width to your desired value
+    }
+    </style>
+    <div class="footer">
+        Digital Wellness Lab 2024<br>
+        Business Analytics, School of Management<br>
+        Kyung Hee University<br>
+        Maintained by H-.M-. Kim & S-.W-. Kim
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 
       
