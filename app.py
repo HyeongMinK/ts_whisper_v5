481b10dd1cb131de1b570de7ddacbcf2fbad7a42
HyeongMinK <aalynm@gmail.com>
2024-07-21 23:11:16 +0900

    v5

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using F
 # Load the Whisper model
 @st.cache_resource
 def load_whisper_model():
-    return WhisperModel(model_size, device="cpu", compute_type="int8")
+    return WhisperModel("small", device="cpu", compute_type="int8")
 
 model = load_whisper_model()
 api_key = os.getenv('OPENAI_API_KEY')  # 환경 변수에서 API 키를 가져옵니다.
