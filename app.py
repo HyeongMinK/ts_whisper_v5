7d9bad2699b665348b6c0fd76d30296ddb2e4da4
HyeongMinK <aalynm@gmail.com>
2024-07-22 18:00:45 +0900

    v4

--- app.py
+++ app.py
@@ -453,7 +453,7 @@ if st.session_state.temp_page == -1:
             ts_text = translator_call(client, all_script, selected_language, selected_tone)
         after_script += ts_text
     st.markdown("#### After translation")
-    st.markdown(f"```\n{after_script}\n```")                
+    st.markdown(f"<div style='white-space: pre-wrap;'>{all_script}</div>", unsafe_allow_html=True)                
 
 st.markdown(
     """
