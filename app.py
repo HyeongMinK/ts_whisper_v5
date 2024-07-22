6812d3009c648f18c04b28a956cc6e1cebe31474
HyeongMinK <aalynm@gmail.com>
2024-07-23 01:09:19 +0900

    v5

--- app.py
+++ app.py
@@ -479,7 +479,7 @@ st.markdown(
         font-size: 12px;   /* 글씨 크기 설정 */
         color: gray;       /* 텍스트 색상 설정 */
         position: relative;/* 상대 위치 설정 */
-        bottom: -100px;     /* 페이지 하단에서 50px 위로 */
+        bottom: -150px;     /* 페이지 하단에서 50px 위로 */
         width: 100%;       /* 너비를 페이지 전체로 설정 */
         text-align: center;/* 가운데 정렬 */
         background-color: white; /* 배경색 설정 */
